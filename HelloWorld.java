e842117b7f2c45519c06ac45abd30b97cb1226cd
jaychotalia <47367380+jaychotalia@users.noreply.github.com>
2020-01-26 12:24:53 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("-----***** Hello wworld & Welcome to Jenkins Training *****-----");
+		System.out.println("-----***** Made some changes *****-----");
 	}
 }
